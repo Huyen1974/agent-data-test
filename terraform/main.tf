e6d0884432e3ab7727a92ab4f37698761e7d02cc
Nguyễn Minh Huyên <nmhuyen@gmail.com>
2025-10-30 21:07:36 +0700

    refactor: rely on pre-existing secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,6 @@ module "agent_data_service_test" {
   location               = "asia-southeast1"
   service_name           = "agent-data-test"
   image_path             = "asia-southeast1-docker.pkg.dev/github-chatgpt-ggcloud/agent-data-test/agent-data-test:latest"
-  api_key_secret         = google_secret_manager_secret.agent_data_api_key.secret_id
-  api_key_secret_version = google_secret_manager_secret_version.agent_data_api_key.version
+  api_key_secret         = data.google_secret_manager_secret.agent_data_api_key.secret_id
+  api_key_secret_version = data.google_secret_manager_secret_version.agent_data_api_key.version
 }
